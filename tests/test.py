57ef59ccf43dbbc924538f451e640e58b3dc6cd4
Andrej Prochazka <hrocho@vodacionline.sk>
2018-01-22 23:54:08 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -75,7 +75,7 @@ class TestStructures(unittest.TestCase):
         self.assertAlmostEqual(m2.molar_mass(),18,1)
         self.assertAlmostEqual(m2.density(),1.0,1)
 
-        m3 = catima.get_material(301)
+        m3 = catima.get_material(3001)
         self.assertEqual(m3.ncomponents(),0)
         self.assertAlmostEqual(m3.molar_mass(),0,1)
         self.assertAlmostEqual(m3.density(),0.0,1)
