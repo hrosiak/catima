76ab42d9e00c4681f9b83cc63cc1f593608b2dc9
Andrej Prochazka <hrocho@vodacionline.sk>
2019-10-22 21:25:52 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,19 +1,8 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import subprocess, os
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+if read_the_docs_build:
+    subprocess.call('doxygen', shell=True)
 
 # -- Project information -----------------------------------------------------
 
