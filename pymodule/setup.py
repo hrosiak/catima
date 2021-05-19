62e50288877c76dbd6e1e3d5eca2e8ea861ee7ca
Andrej Prochazka <apb@gp-li-pyt01.medaustron.internal>
2021-05-19 19:08:59 +0200

    setup.py

--- pymodule/setup.py
+++ pymodule/setup.py
@@ -5,14 +5,16 @@ from setuptools import setup
 
 DIR = Path(__file__).parents[0]
 
-SRC = [str(DIR/'pycatima.cpp')]+[str(fname.resolve()) for fname in DIR.glob('../*.cpp')]
-SRCG = [str(fname.resolve()) for fname in DIR.glob('../global/*.c')]
-SRC += SRCG
+SRC = [str((DIR/'pycatima.cpp').resolve())]#+[str(fname.resolve()) for fname in DIR.glob('../*.cpp')]
+#SRCG = [str(fname.resolve()) for fname in DIR.glob('../global/*.c')]
+#SRC += SRCG
 print (SRC)
 example_module = Pybind11Extension(
     'pycatima',
     SRC,
-    include_dirs=['../build/include','../global']
+    include_dirs=['../build/include','../global'],
+    library_dirs=['../build/lib','../build'],
+    libraries=['catima']
 )
 
 setup(
